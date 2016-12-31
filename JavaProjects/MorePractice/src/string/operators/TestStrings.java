7582c6f043b382a2d368288e4ce2dcbcb67e62e4
tfaneye <tunde_eye@yahoo.com>
2016-12-31 17:43:19 +0000

    update

--- JavaProjects/MorePractice/src/string/operators/TestStrings.java
+++ JavaProjects/MorePractice/src/string/operators/TestStrings.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class TestStrings {
 
     public static void main(String[] args){
+        /*
         String name = "Johnston";
 
         char letter = name.charAt(2);
@@ -24,5 +25,51 @@ public class TestStrings {
         SimpleDateFormat dateFormat = new SimpleDateFormat("E dd/MM/yyyy 'at' hh:mm:ss a zzz");
         String dateTime = dateFormat.format(date);
         System.out.println(dateTime);
+
+        System.out.println("================================================");
+
+        int result = 1 + 2;
+        // result is now 3
+        System.out.print("This is additive example ---> ");
+        System.out.println("1 + 2 = " + result);
+        int original_result = result;
+
+        result = result - 1;
+        // result is now 2
+        System.out.println(original_result + " - 1 = " + result);
+        original_result = result;
+
+        result = result * 2;
+        // result is now 4
+        System.out.println(original_result + " * 2 = " + result);
+        original_result = result;
+
+        result = result / 2;
+        // result is now 2
+        System.out.println(original_result + " / 2 = " + result);
+        original_result = result;
+
+        result = result + 8;
+        // result is now 10
+        System.out.println(original_result + " + 8 = " + result);
+        original_result = result;
+
+        result = result % 7;
+        // result is now 3
+        System.out.println(original_result + " % 7 = " + result);
+
+        System.out.println("================================================");
+        */
+        int i;
+        //++i;*/
+        for(i=0;i<=10;++i)
+        System.out.println(i);
+        /*i++;
+
+        System.out.println(i);
+
+        System.out.println(++i);*/
+
+
     }
 }
