8e3769bd659f4a0bfa06cd84e49681c30f6bc287
tfaneye <tunde_eye@yahoo.com>
2017-01-02 18:57:20 +0000

    update

--- PracticeJavaLang/src/controlStatements/IfElseDemo.java
+++ PracticeJavaLang/src/controlStatements/IfElseDemo.java
@@ -6,7 +6,7 @@ package controlStatements;
 public class IfElseDemo {
 
     public static void main(String[] args){
-        int testScore = 76;
+        int testScore = 45;
         char grade;
 
         if(testScore >=90){
@@ -23,5 +23,34 @@ public class IfElseDemo {
             grade = 'F';
         }
         System.out.println("Grade = " +grade);
+
+        System.out.println("================================================");
+
+        int month = 12;
+        int year = 2017;
+        int numDays = 0;
+
+        switch (month) {
+            case 1: case 3: case 5:
+            case 7: case 8: case 10:
+            case 12:
+                numDays = 31;
+                break;
+            case 4: case 6:
+            case 9: case 11:
+                numDays = 30;
+                break;
+            case 2:
+                if (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0))
+                    numDays = 29;
+                else
+                    numDays = 28;
+                break;
+            default:
+                System.out.println("Invalid month.");
+                break;
+        }
+        System.out.println("Number of Days = "
+                + numDays);
     }
 }
