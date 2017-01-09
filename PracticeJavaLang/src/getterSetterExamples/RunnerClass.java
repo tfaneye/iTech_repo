28789d78dd56f7a17d0ea2f9b8aa49f58cd7f070
tfaneye <tunde_eye@yahoo.com>
2017-01-09 00:13:22 +0000

    update to PracticeJavaLang

---
+++ PracticeJavaLang/src/getterSetterExamples/RunnerClass.java
@@ -0,0 +1,16 @@
+package getterSetterExamples;
+
+/**
+ * Created by inet-tech on 08/01/17.
+ */
+public class RunnerClass {
+
+    public static void main(String[] args) {
+        MyCompany myCompany = new MyCompany();
+
+        myCompany.setEmployeeSalary(50000);
+        myCompany.giveBonus();
+
+
+    }
+}
