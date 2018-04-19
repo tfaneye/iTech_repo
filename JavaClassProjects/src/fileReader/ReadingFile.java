dac90f8c8b5a29d0f37ada9f18d3ed4a121bf1a7
tfaneye <tunde_eye@yahoo.com>
2018-04-19 23:12:54 +0100

    added file reader code

---
+++ JavaClassProjects/src/fileReader/ReadingFile.java
@@ -0,0 +1,22 @@
+package fileReader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Tunde on 13/03/2018.
+ */
+public class ReadingFile {
+
+    private static FileInputStream fis;
+    private static Properties prop;
+
+
+    public static void main(String[] args) throws IOException {
+        fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\resources\\env.properties");
+        prop = new Properties();
+        prop.load(fis);
+        System.out.println(prop);
+    }
+}
