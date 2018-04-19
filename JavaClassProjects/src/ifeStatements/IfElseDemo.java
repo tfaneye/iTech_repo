52db7760d4357ba2d8ef51952b9dc9bc866de033
Tunde Faneye <tunde_eye@yahoo.com>
2018-04-19 23:21:08 +0100

    added string operators

--- JavaClassProjects/src/ifeStatements/IfElseDemo.java
+++ JavaClassProjects/src/ifeStatements/IfElseDemo.java
@@ -17,6 +17,7 @@ public class IfElseDemo {
 
         public static void main(String[] args) {
 
+
         IfElseDemo ifElseDemo = new IfElseDemo();
         int newSpeed = ifElseDemo.applyBrakes();
             System.out.println(newSpeed);
@@ -37,6 +38,9 @@ public class IfElseDemo {
             }
             System.out.println("Grade = " + grade);
 
+
+
+            // Example of for loop
             for(int i=1; i<11; i++){
                 System.out.println("Count is: " + i);
                 if(i==5){
@@ -45,17 +49,37 @@ public class IfElseDemo {
 
             }
 
+            // Example of Advanced for loop or foreach loop
             String[] names = { "John", "Mark", "James", "Matt" };
-            for (String name :
-                 names) {
+            for (String name : names) {
                 System.out.println(name);
-               // System.out.println("==============================");
-                /*if (name.toLowerCase().contains("James".toLowerCase())){
+                System.out.println("==============================");
+                if (name.toLowerCase().contains("James".toLowerCase())){
                     System.out.println(name);
                     break;
-                }*/
+                }
 
             }
+
+            int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+            for (int item : numbers) {
+                System.out.println("Count is: " + item);
+            }
+
+
+
+            String word = "mississipi";
+            int len = word.length();
+            int numSs = 0;
+            for (int i=0; i < len; i++){
+                //we only interested in s
+                if (word.charAt(i) != 's')
+                    continue;
+
+                //number of s increment
+                numSs++;
+            }
+            System.out.println("there are " + numSs + "'s in " + word);
         }
 
 
